0c8b5bda6206a018eb50f472373a5ffd82ac62fc
Simone Lungarella <simonelungarella@gmail.com>
2020-03-18 17:01:35 +0100

    Added applicationContext

--- src/business/ControllerBean.java
+++ src/business/ControllerBean.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 @ManagedBean
+@Component
 public class ControllerBean {
+
+	@Autowired
 	private UserSRV userService;
+	
 	private List<User> resultList = new ArrayList<>();
 
 	public ControllerBean() {
--- src/business/UserSRV.java
+++ src/business/UserSRV.java
@@ -15,13 +15,17 @@ public class UserSRV {
 	@Autowired
 	@Qualifier("DataSource")
 	private DataSource dataSource;
+	
 	Connection conn = null;
 	List<User> usersList;
 	UserDao userD;
 
 	public List<User> getByUsername(String username) {
 		try {
+			System.out.println("Connessione in corso...");
+			System.out.println(dataSource);
 			conn = dataSource.getConnection();
+			System.out.println("Connessione al DB avvenuta con successo!");
 
 			usersList = userD.getByUsername(username, conn);
 		} catch (Exception e) {
